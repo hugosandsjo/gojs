b4b77466074a47170e2d7d8eac863b7cbdf5e36f
hugosandsjo <hugosandsjo@gmail.com>
2024-10-21 15:14:48 +0200

    schemas defined and some simple authentiocation and session fixed

---
+++ prisma/migrations/20241021122845_created_more_optional_fields_to_user/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "User" ALTER COLUMN "location" DROP NOT NULL,
+ALTER COLUMN "instagram" DROP NOT NULL,
+ALTER COLUMN "facebook" DROP NOT NULL,
+ALTER COLUMN "website" DROP NOT NULL;
--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,8 +1,11 @@
 import SignIn from "@/app/components/SignIn";
+import { SignOut } from "@/app/components/SignOut";
+import { auth } from "@/lib/auth";
 import Link from "next/link";
 
-export default function Header() {
-  console.log();
+export default async function Header() {
+  const session = await auth();
+  console.log("Session:", session);
   return (
     <>
       <header className="flex justify-between bg-slate-400 py-6">
@@ -11,7 +14,7 @@ export default function Header() {
           <Link href={"/shop"}>Shop</Link>
           <Link href={"/dashboard"}>Dashboard</Link>
           <Link href={"/about"}></Link>
-          <SignIn />
+          {session ? <SignOut /> : <SignIn />}
         </div>
         <Link href={"/"}>
           <h1 className="text-6xl font-serif">Gojs app</h1>
