4f8051ce92f95f880295278527aa7c569413c7d9
hugosandsjo <hugosandsjo@gmail.com>
2024-10-24 13:29:48 +0200

    changed prisma schema instead

--- src/app/shop/[id]/page.tsx
+++ src/app/shop/[id]/page.tsx
@@ -4,30 +4,45 @@ import { useEffect, useState } from "react";
 import { Product } from "@prisma/client";
 import ProductParagraph from "@/app/components/typography/ProductParagraph";
 import Link from "next/link";
+import Image from "next/image";
+import { getImgixUrl } from "@/lib/utils";
 
 export default function SingleProduct({ params }: { params: { id: string } }) {
-  const [product, setProduct] = useState<Product | null>(null);
+  const [product, setProduct] = useState<
+    (Product & { image_url: string | null }) | null
+  >(null);
 
   useEffect(() => {
     (async () => {
       const data = await getProduct(params.id);
-      setProduct(data);
+      let imgUrl: string | null = null; // Initialize as null
+
+      if (data?.image_key) {
+        imgUrl = getImgixUrl(data.image_key, {
+          w: "400",
+          h: "400",
+          fit: "crop",
+          auto: "format",
+        });
+      }
+
+      setProduct(data ? { ...data, image_url: imgUrl } : null);
     })();
   }, [params.id]);
 
   if (!product) {
     return <p>Loading...</p>;
   }
-
   return (
     <section className="flex flex-col items-center gap-8 p-10">
-      {/* Product Title */}
       <h1 className="text-3xl font-bold">{product.title}</h1>
 
       {product.image_url ? (
-        <img
+        <Image
           src={product.image_url}
           alt={product.title}
+          width={400}
+          height={400}
           className="w-80 h-80 object-cover"
         />
       ) : (
@@ -36,7 +51,6 @@ export default function SingleProduct({ params }: { params: { id: string } }) {
         </div>
       )}
 
-      {/* Product Details */}
       <div className="text-lg flex flex-col gap-4">
         <ProductParagraph>Price: ${product.price}</ProductParagraph>
         <ProductParagraph>
